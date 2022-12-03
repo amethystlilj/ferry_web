0e36076d70b9875411d1a4b92ca6f402a4cbd2b3
liliangjie <amethystlilj@gmail.com>
2022-12-04 04:03:07 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,48 +1,32 @@
 import Vue from 'vue'
-import VueI18n from 'vue-i18n'
-
 import Cookies from 'js-cookie'
-
 import 'normalize.css/normalize.css' // a modern alternative to CSS resets
-
 import Element from 'element-ui'
+import VueI18n from 'vue-i18n'
+import enLocale from 'element-ui/lib/locale/lang/en'
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN' // fontAwesome 图标库样式
 import './styles/element-variables.scss'
-
 import '@/styles/index.scss' // global css
 import '@/styles/admin.scss'
-
 import App from './App'
 import store from './store'
 import router from './router'
 import permission from './directive/permission'
-
-import { parseTime, resetForm, addDateRange, selectDictLabel } from '@/utils/costum'
-
 import './icons' // icon
 import './permission' // permission control
-
 import * as filters from './filters' // global filters
-
 import Pagination from '@/components/Pagination'
-
 import VueParticles from 'vue-particles'
-Vue.use(VueParticles)
-
 // 代码编辑器
 import VueCodeMirror from 'vue-codemirror'
 import 'codemirror/lib/codemirror.css'
-Vue.use(VueCodeMirror)
-
 // 富文本编辑器
 import VueEditor from 'vue2-editor'
-Vue.use(VueEditor)
-
 import iconPicker from 'e-icon-picker'
 import 'e-icon-picker/dist/index.css'// 基础样式
 import 'e-icon-picker/dist/main.css'
-import enLocale from 'element-ui/lib/locale/lang/en'
-import zhLocale from 'element-ui/lib/locale/lang/zh-CN' // fontAwesome 图标库样式
-Vue.use(iconPicker)
+import FormMaking from 'form-making'
+import { parseTime, resetForm, addDateRange, selectDictLabel } from '@/utils/costum'
 
 // 全局方法挂载
 Vue.prototype.parseTime = parseTime
@@ -65,12 +49,12 @@ Vue.prototype.msgInfo = function(msg) {
   this.$message.info(msg)
 }
 
+Vue.use(VueParticles)
+Vue.use(VueCodeMirror)
+Vue.use(VueEditor)
+Vue.use(iconPicker)
 Vue.use(permission)
-
-Vue.use(Element, {
-  i18n: (key, value) => i18n.t(key, value),
-  size: Cookies.get('size') || 'medium' // set element-ui default size
-})
+Vue.use(VueI18n)
 
 const messages = {
   'en-US': {
@@ -79,7 +63,8 @@ const messages = {
       document: 'Docs',
       pricing: 'Pricing',
       advanced: 'Advanced'
-    }
+    },
+    ...enLocale
   },
   'zh-CN': {
     header: {
@@ -87,27 +72,23 @@ const messages = {
       document: '使用文档',
       pricing: '商业授权',
       advanced: '高级版本'
-    }
+    },
+    ...zhLocale
   }
 }
 
+// Create VueI18n instance with options
 const i18n = new VueI18n({
-  messages: {
-    'en-US': {
-      ...enLocale, ...messages['en-US']
-    },
-    'zh-CN': {
-      ...zhLocale, ...messages['zh-CN']
-    }
-  }
+  locale: 'zh-CN', // set locale
+  messages // set locale messages
 })
 
-import FormMaking from 'form-making'
-// import 'form-making/dist/FormMaking.css'
-// Vue.use(FormMaking)
-Vue.use(FormMaking, {
-  i18n
+Vue.use(Element, {
+  i18n: (key, value) => i18n.t(key, value),
+  size: Cookies.get('size') || 'medium' // set element-ui default size
 })
+
+Vue.use(FormMaking, { lang: 'zh-CN', i18n })
 
 // register global utility filters
 Object.keys(filters).forEach(key => {
