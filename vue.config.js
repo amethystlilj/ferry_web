6da15c97fb4cae01db03f3ca293a73d29abf1bfb
liliangjie <amethystlilj@gmail.com>
2024-03-16 00:01:27 +0800

    feat():升级部分依赖

--- vue.config.js
+++ vue.config.js
@@ -47,6 +47,9 @@ module.exports = {
     resolve: {
       alias: {
         '@': resolve('src')
+      },
+      fallback:{
+        path: require.resolve("path-browserify")
       }
     }
   },
@@ -91,14 +94,6 @@ module.exports = {
     config
       .when(process.env.NODE_ENV !== 'development',
         config => {
-          config
-            .plugin('ScriptExtHtmlWebpackPlugin')
-            .after('html')
-            .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
-              inline: /runtime\..*\.js$/
-            }])
-            .end()
           config
             .optimization.splitChunks({
               chunks: 'all',
